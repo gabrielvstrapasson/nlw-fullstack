c572ac55e525d5c65c2e23edffa2f89ceb7da2c7
AA-Gabriel <gabri.vstrapasson@gmail.com>
2025-02-18 11:36:58 -0300

    NLW - FULLSTACK part 2

--- index.js
+++ index.js
@@ -1,9 +1,81 @@
 const app = document.getElementById("app")
+const users = [
+  {
+    email: 'test@test.com',
+    phone: '999999990',
+    ref: 100,
+    refby: null
+  },
+  {
+    email: 'tust@tust.com',
+    phone: '9999999999',
+    ref: 200,
+    refBy: 200
+  },
+    {
+    email: 'tost@tost.com',
+    phone: '9999999999',
+    ref: 300,
+    refBy: 100
+  },
+]
+
+const getUser = (userData) =>{
+  return users.find((user) => {
+    return user.email == userData.email
+  })
+}
+
+const getTotalSubscribers = (userData) =>{
+  const subs = users.filter((user) => {
+    return user.refBy == userData.ref
+  })
+  return subs.length
+}
+
+const showInvite = (userData) => {
+  app.innerHTML = `
+        <input type="text" id="link" value="https://evento.com?ref=${userData.ref}" disabled>
+    <div id="stats">
+      <h4>
+        ${getTotalSubscribers(userData)}
+      </h4>
+    <p>
+      Inscrições feitas
+    </p>
+    </div>
+  `
+}
+
+const saveUser = (userData) => {
+  const newUser = {
+    ...userData,
+    ref: Math.rond(Math.random() * 4000),
+    refBy: 100
+  }
+
+  users.push(newUser)
+  console.log(users)
+  return newUser
+}
 
 const formAction = () => {
     const form = document.getElementById('form')
     form.onsubmit = (event) => {
         event.preventDefault()
+        const formData = new FormData(form)
+        const userData = {
+          email: formData.get('email'),
+          phone: formData.get('phone')
+        }
+
+        const user = getUser(userData)
+        if (user){
+          showInvite(user)
+        } else {
+          const newUser = saveUser(userData)
+            showInvite(newUser)
+        }        
     }
 }
 
@@ -24,3 +96,5 @@ const startApp = () => {
 }
 
 startApp()
+
+document.getElementedById("logo").onclick = () => startApp()
